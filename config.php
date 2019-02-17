41f329a2a89f88d84a41a5e2798e52e0052e9868
rudenskaya <rudenskaya96@yandex.ru>
2019-02-17 13:02:51 +0300

    Добавила главную страницу

--- config.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $link = mysqli_connect('localhost', 'root', '', '24092018_3project');
-    mysqli_set_charset($link, 'utf8');
--- index.php
+++ index.php
@@ -1,16 +1,81 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Проект #3</title>
-    <link rel="stylesheet" href="/style/style.css">
-</head>
-<body>
-    <h1>Код проекта №3</h1>
-    <p>Новый параграф</p>
-    <p>Новый параграф 2</p>
-    <script src="/js/main.js"></script>
-</body>
-</html>
+
+<?php include($_SERVER['DOCUMENT_ROOT']."/header.php"); ?>
+
+<div class="wrapper">
+    <div class="wrapper_text_top">
+        <h1>Новые поступления весны</h1>
+        <p>Мы подготовили для вас лучшие новинки сезона</p>
+    </div>
+    
+
+    <button class="wrapper_btn">Посмотреть новинки</button>
+
+    <div class="main_block">
+        <div class="block">
+            <div class="img1">
+                <p class="text">ДЖИНСОВЫЕ КУРТКИ</p> 
+                <br>
+                <p class="text_italic">NEW ARRIVAL</p>
+            </div>
+            <div class="img5"></div>
+        </div>
+
+        <div class="block">
+            <div class="block_grey">
+                <div class="attention"></div>
+                <p>каждый сезон мы подготавливаем для Вас исключительно лучшую модную одежду. Следите за нашими новинками.</p> 
+            </div>
+            <div class="img2">
+                <p class="text">ДЖИНСЫ</p> 
+                <br>
+                <p class="text_italic">от 3200руб.</p>
+            </div>
+            <div class="block_dark_grey">
+                <div class="left"></div>
+                <p class="text">АКСЕССУАРЫ</p> 
+            </div>
+        </div>
+
+        <div class="block">
+            <div class="img3"></div>
+            <div class="block_grey">
+                <div class="attention"></div>
+                <p>Самые низкие цены в Москве. Нашли дешевле? Вернем разницу.</p> 
+            </div>
+            <div class="img6">
+                <p class="text">СПОРТИВНАЯ ОДЕЖДА</p> 
+                <br>
+                <p class="text_italic">от 590руб.</p>
+            </div>
+        </div>
+
+        <div class="block">
+            <div class="block_dark_grey">
+                <div class="left"></div>
+                <p class="text">ЭЛЕГАНТНАЯ ОБУВЬ </p> 
+                <br>
+                <p class="text_italic">БОТИНКИ, КРОССОВКИ</p>
+            </div>
+            <div class="img4">
+                <p class="text">ДЕТСКАЯ ОДЕЖДА</p> 
+                <br>
+                <p class="text_italic">NEW ARRIVAL</p>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper_text_bottom">
+        <h2>Будь всегда в курсе выгодных предложений</h2>
+        <p>подписывайся и следи за новинками и выгодными предложениями</p>
+    </div>
+   
+    <div class="form_bottom">
+        <form action="#" >
+            <input type="email" name="email" placeholder="e-mail">
+            <button class="form_btn">записаться</button>
+        </form>
+        <p>Некорректный e-mail. Попробуйте еще раз.</p>
+    </div>
+    
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT']."/footer.php"); ?>
